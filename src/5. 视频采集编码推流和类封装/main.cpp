b1598f590ca48362f97e167aa259c8f592575e38
gebaoyao <gebaoyao@sina.com>
2019-12-17 10:50:48 +0800

    2. 基于opencv采集视频流并使用异常处理失败流程

--- src/5. 视频采集编码推流和类封装/main.cpp
+++ src/5. 视频采集编码推流和类封装/main.cpp
@@ -1,34 +1,36 @@
 #include "opencv2/highgui.hpp"
 #include <iostream>
-#pragma comment(lib,"opencv_world320d.lib")
+#pragma comment(lib,"opencv_world320.lib")
 using namespace std;
 using namespace cv;
 int main(int argc, char *argv[])
 {
 	//海康相机的rtsp url
 	//char *inUrl = "rtsp://test:test123456@192.168.1.64";
+	//直播服务器（nginx-rtmp）的rtmp
+	char *outnUrl = "rtmp://192.168.1.6/live";
 	VideoCapture cam;
-	namedWindow("video");
-
-	//if(cam.open(inUrl))
-	if (cam.open(0))
-	{
-		cout << "open camera success!" << endl;
-	}
-	else
-	{
-		cout << "open camera failed!" << endl;
-
-		waitKey(1);
-		return -1;
-	}
 	Mat frame;
-	for (;;)
+	namedWindow("video");
+		
+	try
 	{
-		cam.read(frame);
-		imshow("video", frame);
-		waitKey(1);
+		cam.open(0);
+		if (!cam.isOpened()) throw exception("cam open failed");
+		cout << "cam open succeed" << endl;
+		
+		for (;;)
+		{
+			if (!cam.grab()) continue;///读取视频帧，解码视频帧
+			if (!cam.retrieve(frame)) continue;///yuv转换为rgb
+			imshow("video", frame);
+			waitKey(1);
+		}
 	}
-
+	catch (exception&ex)
+	{
+		if (cam.isOpened()) cam.release();
+		cerr << ex.what() << endl;
+	}
 	return 0;
 }
