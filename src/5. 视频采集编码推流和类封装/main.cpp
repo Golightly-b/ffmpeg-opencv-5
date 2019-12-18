7a0e8ba32be1331288bbd4bc2c9d7b48b74dff64
gebaoyao <gebaoyao@sina.com>
2019-12-18 17:12:06 +0800

    5&6.查找、创建、配置、打开编码器

--- src/5. 视频采集编码推流和类封装/main.cpp
+++ src/5. 视频采集编码推流和类封装/main.cpp
@@ -12,26 +12,29 @@ using namespace std;
 using namespace cv;
 int main(int argc, char *argv[])
 {
-	//海康相机的rtsp url
-	//char *inUrl = "rtsp://test:test123456@192.168.1.64";
-	//直播服务器（nginx-rtmp）的rtmp
-	char *outnUrl = "rtmp://192.168.1.6/live";
+	//char *inUrl = "rtsp://test:test123456@192.168.1.64";//海康相机的rtsp url
+	char *outnUrl = "rtmp://192.168.1.6/live";//直播服务器（nginx-rtmp）的rtmp
 	VideoCapture cam;
 	Mat frame;
 	namedWindow("video");
 	SwsContext *vsc = NULL;//像素格式转换上下文
 	AVFrame *yuv = NULL;//输出的数据结构
+	AVCodecContext *vc = NULL;//编码器上下文
+	
+	avcodec_register_all();//注册所有的编解码器
+
+	////////////////////////////////////////////////////////////////
 	try
-	{
-		////////////////////////////////////////////////////////////////
+	{		
 		/// 1 使用opencv打开rtsp相机
 		cam.open(0);
 		if (!cam.isOpened()) throw exception("cam open failed");
-		cout << "cam open succeed" << endl;
-		///2 初始化格式转换上下文
+		cout << "cam open succeed" << endl;		
 		int inWidth = cam.get(CAP_PROP_FRAME_WIDTH);
 		int inHeight = cam.get(CAP_PROP_FRAME_HEIGHT);
-		int fps = cam.get(CAP_PROP_FPS);
+		int fps = 29;//cam.get(CAP_PROP_FPS)
+		
+		///2 初始化格式转换上下文
 		vsc = sws_getCachedContext(vsc,
 			inWidth, inHeight, AV_PIX_FMT_BGR24,	 //源宽、高、像素格式
 			inWidth, inHeight, AV_PIX_FMT_YUV420P,//目标宽、高、像素格式
@@ -39,7 +42,7 @@ int main(int argc, char *argv[])
 			0, 0, 0);
 		if (!vsc) throw exception("sws_getCachedContext failed!");
 
-		//输出的数据结构
+		///3 初始化输出的数据结构
 		yuv = av_frame_alloc();
 		yuv->format = AV_PIX_FMT_YUV420P;
 		yuv->width = inWidth;
@@ -53,23 +56,60 @@ int main(int argc, char *argv[])
 			throw exception(buf);
 		}
 
+		///4 初始化编码上下文
+		//a 找到编码器
+		AVCodec *codec = avcodec_find_encoder(AV_CODEC_ID_H264);
+		if (!codec)	throw exception("Can`t find h264 encoder!");
+		
+		//b 创建编码器上下文
+		vc = avcodec_alloc_context3(codec);
+		if (!vc)	throw exception("avcodec_alloc_context3 failed!");
+		
+		//c 配置编码器参数
+		vc->flags |= AV_CODEC_FLAG_GLOBAL_HEADER; //全局参数
+		vc->codec_id = codec->id;
+		vc->thread_count = 8;
+		vc->bit_rate = 50 * 1024 * 8;//压缩后每秒视频的bit位大小 50kB
+		vc->width = inWidth;
+		vc->height = inHeight;
+		vc->time_base = { 1,fps };
+		vc->framerate = { fps,1 };
+		//画面组的大小，多少帧一个关键帧
+		vc->gop_size = 50;
+		vc->max_b_frames = 0;
+		vc->pix_fmt = AV_PIX_FMT_YUV420P;
+		
+		//d 打开编码器上下文
+		ret = avcodec_open2(vc, 0, 0);
+		if (ret != 0)
+		{
+			char buf[1024] = { 0 };
+			av_strerror(ret, buf, sizeof(buf) - 1);
+			throw exception(buf);
+		}
+		cout << "avcodec_open2 success!" << endl;
+
 		for (;;)
 		{
-			if (!cam.grab()) continue;///读取视频帧，解码视频帧
-			if (!cam.retrieve(frame)) continue;///yuv转换为rgb
+			///读取视频帧，解码视频帧
+			if (!cam.grab()) continue;
+			///yuv转换为rgb
+			if (!cam.retrieve(frame)) continue;
 			imshow("video", frame);
 			waitKey(1);
+
 			//rgb to yuv
 			//输入的数据结构     交错存放bgr
 			uint8_t *indata[AV_NUM_DATA_POINTERS] = { 0 };
 			indata[0] = frame.data;
 			int insize[AV_NUM_DATA_POINTERS] = { 0 };//一行数据（宽）的字节数
 			insize[0] = frame.cols*frame.elemSize();
-
 			int h = sws_scale(vsc, indata, insize, 0, frame.rows,
 				yuv->data, yuv->linesize);
 			if (h <= 0) continue;
-			cout << h << "" << flush;
+			//cout << h << "" << flush;
+
+
 		}
 	}
 	catch (exception&ex)
@@ -79,6 +119,10 @@ int main(int argc, char *argv[])
 		{
 			sws_freeContext(vsc);
 			vsc = NULL;
+		}
+		if (vc)
+		{
+			avcodec_free_context(&vc);
 		}
 		cerr << ex.what() << endl;
 	}
