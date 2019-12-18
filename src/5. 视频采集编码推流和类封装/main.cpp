97469b5d6cac8fb2de7a4fcc135ad50f106a4cf6
gebaoyao <gebaoyao@sina.com>
2019-12-18 10:14:40 +0800

    4. rgb转yuv空间分配和转换函数调用

--- src/5. 视频采集编码推流和类封装/main.cpp
+++ src/5. 视频采集编码推流和类封装/main.cpp
@@ -2,9 +2,12 @@
 #include <iostream>
 extern "C"{
 #include <libswscale/swscale.h>
+#include "libavcodec/avcodec.h"
 }
 #pragma comment(lib, "swscale.lib")
 #pragma comment(lib,"opencv_world320d.lib")
+#pragma comment(lib,"avcodec.lib")
+#pragma comment(lib,"avutil.lib")
 using namespace std;
 using namespace cv;
 int main(int argc, char *argv[])
@@ -17,7 +20,7 @@ int main(int argc, char *argv[])
 	Mat frame;
 	namedWindow("video");
 	SwsContext *vsc = NULL;//像素格式转换上下文
-
+	AVFrame *yuv = NULL;//输出的数据结构
 	try
 	{
 		////////////////////////////////////////////////////////////////
@@ -36,12 +39,37 @@ int main(int argc, char *argv[])
 			0, 0, 0);
 		if (!vsc) throw exception("sws_getCachedContext failed!");
 
+		//输出的数据结构
+		yuv = av_frame_alloc();
+		yuv->format = AV_PIX_FMT_YUV420P;
+		yuv->width = inWidth;
+		yuv->height = inHeight;
+		yuv->pts = 0;
+		//分配yuv空间
+		int ret = av_frame_get_buffer(yuv, 32);
+		if (ret != 0) {
+			char buf[1024] = { 0 };
+			av_strerror(ret, buf, sizeof(buf) - 1);
+			throw exception(buf);
+		}
+
 		for (;;)
 		{
 			if (!cam.grab()) continue;///读取视频帧，解码视频帧
 			if (!cam.retrieve(frame)) continue;///yuv转换为rgb
 			imshow("video", frame);
 			waitKey(1);
+			//rgb to yuv
+			//输入的数据结构     交错存放bgr
+			uint8_t *indata[AV_NUM_DATA_POINTERS] = { 0 };
+			indata[0] = frame.data;
+			int insize[AV_NUM_DATA_POINTERS] = { 0 };//一行数据（宽）的字节数
+			insize[0] = frame.cols*frame.elemSize();
+
+			int h = sws_scale(vsc, indata, insize, 0, frame.rows,
+				yuv->data, yuv->linesize);
+			if (h <= 0) continue;
+			cout << h << "" << flush;
 		}
 	}
 	catch (exception&ex)
