de5305727fe40ea213034ef167541f1f1f4fb017
sunhaixin <gebaoyao@sina.com>
2019-12-23 10:31:38 +0800

    13&14&15. InitVideoCodec封装、XRtmp接口类

--- src/5. 视频采集编码推流和类封装/XMediaEncode.h
+++ src/5. 视频采集编码推流和类封装/XMediaEncode.h
@@ -1,27 +1,41 @@
 #pragma once
+
 struct AVFrame;
+struct AVPacket;
+class AVCodecContext;
 ///音视频编码接口类
 class XMediaEncode
 {
 public:
+
+	///输入参数
 	int inWidth = 640;
 	int inHeight = 480;
 	int inPixSize = 3;
 
+	///输出参数
 	int outWidth = 640;
 	int outHeight = 480;
+	int bitrate = 4000000;//压缩后每秒视频的bit位大小 50kB
+	int fps = 29;
 
-
-	
 	//工厂生产方法
 	static XMediaEncode * Get(unsigned char index = 0);
-	//初始化像素格式转换的上下文
+
+	//初始化像素格式转换的上下文初始化
 	virtual bool InitScale() = 0;
 
-	virtual AVFrame* RGBtoYUV(char *rgb) = 0;
+	virtual AVFrame* RGBToYUV(char *rgb) = 0;
+
+	//视频编码器初始化
+	virtual bool InitVideoCodec() = 0;
+
+	//视频编码
+	virtual AVPacket * EncodeVideo(AVFrame* frame) = 0;
 
 	virtual ~XMediaEncode();
 
+	AVCodecContext *vc = 0;	//编码器上下文
 protected:
 	XMediaEncode();
 };
--- src/5. 视频采集编码推流和类封装/main.cpp
+++ src/5. 视频采集编码推流和类封装/main.cpp
@@ -1,163 +1,104 @@
-#include "opencv2/highgui.hpp"
+#include <opencv2/highgui.hpp>
 #include <iostream>
-#include "XMediaEncode.h"
-extern "C" {
-#include "libavcodec/avcodec.h"
-#include "libavformat/avformat.h"
-}
 
-#pragma comment(lib,"opencv_world320d.lib")
-#pragma comment(lib,"avformat.lib")
+#include "XMediaEncode.h"
+#include "XRtmp.h"
+
+#pragma comment(lib,"opencv_world320.lib")
 using namespace std;
 using namespace cv;
 int main(int argc, char *argv[])
 {
-	//char *inUrl = "rtsp://test:test123456@192.168.1.64";//海康相机的rtsp url
-	char *outnUrl = "rtmp://192.168.1.111/live";//直播服务器（nginx-rtmp）的rtmp
+	//海康相机的rtsp url
+	//char *inUrl = "rtsp://test:test123456@192.168.1.109";
+	//nginx-rtmp 直播服务器rtmp推流URL
+	char *outUrl = "rtmp://192.168.1.44/live";
+
+
+	//编码器和像素格式转换
+	XMediaEncode *me = XMediaEncode::Get(0);
+
+	//封装和推流对象
+	XRtmp *xr = XRtmp::Get(0);
+
 	VideoCapture cam;
 	Mat frame;
 	namedWindow("video");
-	
-	XMediaEncode *me = XMediaEncode::Get(0);
-	
-	AVCodecContext *vc = NULL;//编码器上下文
-	AVFormatContext *ic = NULL;//rtmp flv 封装器
-	
-	
-	av_register_all();//注册所有的封装器
-	avformat_network_init();//注册所有网络协议
-	////////////////////////////////////////////////////////////////
+
+
+
+
 	int ret = 0;
 	try
-	{		
+	{	////////////////////////////////////////////////////////////////
 		/// 1 使用opencv打开rtsp相机
 		cam.open(0);
-		if (!cam.isOpened()) throw exception("cam open failed");
-		cout << "cam open succeed" << endl;		
+		if (!cam.isOpened())
+		{
+			throw exception("cam open failed!");
+		}
+		cout << " cam open success" << endl;
 		int inWidth = cam.get(CAP_PROP_FRAME_WIDTH);
 		int inHeight = cam.get(CAP_PROP_FRAME_HEIGHT);
-		int fps = 29;//cam.get(CAP_PROP_FPS)
-		
-		///2.初始化格式转换上下文
-		///3.初始化输出的数据结构
+		int fps = cam.get(CAP_PROP_FPS);
+
+		///2 初始化格式转换上下文
+		///3 初始化输出的数据结构
 		me->inWidth = inWidth;
 		me->inHeight = inHeight;
-		me->outHeight = inHeight;
 		me->outWidth = inWidth;
+		me->outHeight = inHeight;
 		me->InitScale();
-		
 
 		///4 初始化编码上下文
 		//a 找到编码器
-		AVCodec *codec = avcodec_find_encoder(AV_CODEC_ID_H264);
-		if (!codec)	throw exception("Can`t find h264 encoder!");
-		
-		//b 创建编码器上下文
-		vc = avcodec_alloc_context3(codec);
-		if (!vc)	throw exception("avcodec_alloc_context3 failed!");
-		
-		//c 配置编码器参数
-		vc->flags |= AV_CODEC_FLAG_GLOBAL_HEADER; //全局参数
-		vc->codec_id = codec->id;
-		vc->thread_count = 8;
-		vc->bit_rate = 50 * 1024 * 8;//压缩后每秒视频的bit位大小 50kB
-		vc->width = inWidth;
-		vc->height = inHeight;
-		vc->time_base = { 1,fps };
-		vc->framerate = { fps,1 };
-		//画面组的大小，多少帧一个关键帧
-		vc->gop_size = 50;
-		vc->max_b_frames = 0;
-		vc->pix_fmt = AV_PIX_FMT_YUV420P;
-		
-		//d 打开编码器上下文
-		ret = avcodec_open2(vc, 0, 0);
-		if (ret != 0)
+		if (!me->InitVideoCodec())
 		{
-			char buf[1024] = { 0 };
-			av_strerror(ret, buf, sizeof(buf) - 1);
-			throw exception(buf);
-		}
-		cout << "avcodec_open2 success!" << endl;
-
-		///5.输出封装器和视频流配置
-		//a.创建封装器上下文
-		ret = avformat_alloc_output_context2(&ic, 0, "flv", outnUrl);
-		if (ret != 0)
-		{
-			char buf[1024] = { 0 };
-			av_strerror(ret, buf, sizeof(buf) - 1);
-			throw exception(buf);
-		}
-		//b.添加视频流
-		AVStream *vs = avformat_new_stream(ic, NULL);
-		if (!vs) throw exception("avformat_new_stream failed");
-		vs->codecpar->codec_tag = 0;
-		//从编码器复制参数
-		avcodec_parameters_from_context(vs->codecpar, vc);
-		av_dump_format(ic, 0, outnUrl, 1);
-
-		///6.打开rtmp的网络输出IO
-		ret = avio_open(&ic->pb, outnUrl,AVIO_FLAG_WRITE);
-		if (ret != 0)
-		{
-			char buf[1024] = { 0 };
-			av_strerror(ret, buf, sizeof(buf) - 1);
-			throw exception(buf);
-		}
-		//写入封装头
-		ret = avformat_write_header(ic, NULL);
-		if (ret != 0)
-		{
-			char buf[1024] = { 0 };
-			av_strerror(ret, buf, sizeof(buf) - 1);
-			throw exception(buf);
+			throw exception("InitVideoCodec failed!");
 		}
 
-		AVPacket pack;
-		memset(&pack, 0, sizeof(pack));
-		int vpts = 0;
-		
+		///5 输出封装器和视频流配置
+		xr->Init(outUrl);
+
+		//添加视频流 
+		xr->AddStream(me->vc);
+		xr->SendHead();
 
 		for (;;)
 		{
-			///读取视频帧，解码视频帧
-			if (!cam.grab()) continue;
+			///读取rtsp视频帧，解码视频帧
+			if (!cam.grab())
+			{
+				continue;
+			}
 			///yuv转换为rgb
-			if (!cam.retrieve(frame)) continue;
-			imshow("video", frame);
-			waitKey(1);
+			if (!cam.retrieve(frame))
+			{
+				continue;
+			}
+			//imshow("video", frame);
+			//waitKey(1);
 
-			//rgb to yuv
+
+			///rgb to yuv
 			me->inPixSize = frame.elemSize();
-			AVFrame *yuv = me->RGBtoYUV((char *)frame.data);
+			AVFrame *yuv = me->RGBToYUV((char*)frame.data);
 			if (!yuv) continue;
 
-
 			///h264编码
-			yuv->pts = vpts;
-			vpts++;
-			ret = avcodec_send_frame(vc, yuv);
-			if (ret != 0)continue;
-			ret = avcodec_receive_packet(vc, &pack);
-			//cout << "*" << pack.size << flush;
+			AVPacket *pack = me->EncodeVideo(yuv);
+			if (!pack) continue;
 
-			pack.pts = av_rescale_q(pack.pts, vc->time_base, vs->time_base);
-			pack.dts = av_rescale_q(pack.dts, vc->time_base, vs->time_base);
-			ret = av_interleaved_write_frame(ic, &pack);
-			if (ret == 0) cout << "#" << flush;
+			xr->SendFrame(pack);
+
 
 		}
+
 	}
-	catch (exception&ex)
+	catch (exception &ex)
 	{
-		if (cam.isOpened()) cam.release();
-		
-		if (vc)
-		{
-			avio_closep(&ic->pb);
-			avcodec_free_context(&vc);
-		}
+		if (cam.isOpened())
+			cam.release();
 		cerr << ex.what() << endl;
 	}
 	getchar();
